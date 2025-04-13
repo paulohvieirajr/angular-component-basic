2fd0e05506ccfb274206ea64f36ba7379c1bb596
paulohvieirajr <paulohvieirajr@gmail.com>
2025-04-13 17:05:49 -0300

    estrutura inicial do component button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component, VERSION } from '@angular/core';
 
 import { HelloComponent } from './hello.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [HelloComponent, MenuComponent],
+  imports: [HelloComponent, MenuComponent, ButtonComponent],
 })
 export class AppComponent {
   name = 'Angular ' + VERSION.major;
---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-button',
+    templateUrl: './button.component.html',
+    styleUrls: ['./button.component.css']
+})
+
+export class ButtonComponent {
+
+}
